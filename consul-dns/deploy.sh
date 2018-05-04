334538563c977cd9098d8893bd69a572e8fbb964
Wolf Oliver <owolf@anynines.com>
2018-05-04 16:17:53 +0200

    fix consul-dns/deploy.sh

--- consul-dns/deploy.sh
+++ consul-dns/deploy.sh
@@ -1,1 +1,3 @@
- bosh deploy -n -d consul-dns unzipped-ds-release/anynines-deployment/consul-dns/consul-dns.yml -l unzipped-ds-release/anynines-deployment/iaas-config/bosh-lite.yml 
+#!/bin/bash
+
+bosh deploy -n -d consul-dns unzipped-ds-release/anynines-deployment/consul-dns/consul-dns.yml -l unzipped-ds-release/anynines-deployment/iaas-config/bosh-lite.yml
