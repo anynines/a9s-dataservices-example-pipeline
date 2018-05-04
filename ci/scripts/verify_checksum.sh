2d3076ee0cc308a66adc8f8d5bbf1dc7cddbc90a
Wolf Oliver <owolf@anynines.com>
2018-05-04 14:34:23 +0200

    make working directory for checksum script configurable

--- ci/scripts/verify_checksum.sh
+++ ci/scripts/verify_checksum.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd $RELEASE_DIR
+
 VERSION=`ls anynines-deployment-*.tgz | cut -d'-' -f 3 | cut -d'.' -f1,2,3`
 EXPECTED_CHECKSUM=`curl "https://s3-eu-west-1.amazonaws.com/anynines-bosh-releases/anynines-deployment-${VERSION}.tgz.sha256" | cut -d' ' -f 1`
 ACTUAL_CHECKSUM=`sha256sum anynines-deployment-*.tgz | cut -d' ' -f 1`
@@ -12,3 +14,5 @@ else
   echo "anynines deployment offline release (version ${VERSION}) checksum is invalid (expected to be ${EXPECTED_CHECKSUM} but is ${ACTUAL_CHECKSUM})"
   exit 1
 fi
+
+popd
