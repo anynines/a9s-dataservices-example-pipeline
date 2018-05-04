d88ee2ac4de190169481dd9a6a92e0b3d99d5747
Wolf Oliver <owolf@anynines.com>
2018-05-04 22:27:50 +0200

    fix postgresql-service/deploy.sh

--- postgresql-service/deploy.sh
+++ postgresql-service/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bosh deploy -n -d a9s-pg unzipped-ds-release/anynines-deployment/postgresql-service/postgresql-service.yml -l unzipped-ds-release/anynines-deployment/iaas-config/bosh-lite.yml
+bosh deploy -n -d postgresql-service unzipped-ds-release/anynines-deployment/postgresql-service/postgresql-service.yml -l unzipped-ds-release/anynines-deployment/iaas-config/bosh-lite.yml
